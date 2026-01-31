f40c6be6b1e307a8f13d28d28bb726e6a1508e45
Danprestige <danprestige4real2@gmail.com>
2026-01-31 21:55:33 +0100

    Add list_books view and template for ALX test

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,40 +1,22 @@
 from django.db import models
 
-
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(
-        Author,
-        on_delete=models.CASCADE,
-        related_name='books'
-    )
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.PositiveIntegerField(null=True, blank=True)
+    libraries = models.ManyToManyField('Library', related_name='books')  # string reference
 
     def __str__(self):
         return self.title
-
-
-class Library(models.Model):
-    name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
-
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(
-        Library,
-        on_delete=models.CASCADE,
-        related_name='librarian'
-    )
-
-    def __str__(self):
-        return self.name
