328a75c407e0f7330b1480a4ff9eaa266462cbf8
Danprestige <danprestige4real2@gmail.com>
2026-01-31 22:22:03 +0100

    ALX exact keyword fix

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -5,3 +5,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
 ]
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
+
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
