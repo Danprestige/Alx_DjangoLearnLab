7c4b938c215c6b12e74b2dc02fc582bfe35db9e6
Danprestige <danprestige4real2@gmail.com>
2026-02-21 08:19:48 +0100

    Set up Django project with DRF and Book model

--- advanced_features_and_security/bookshelf/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ExampleForm(forms.Form):
-    # Form validation sanitizes input automatically
-    book_title = forms.CharField(max_length=100)
-    author_name = forms.CharField(max_length=100)
